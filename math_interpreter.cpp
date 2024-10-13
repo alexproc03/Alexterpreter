ef19cfb75ff75e548acfffe537443030a015af92
Alexander <alexprosd@gmail.com>
2024-10-12 22:25:29 -0700

    add if statements and while loops

--- math_interpreter.cpp
+++ math_interpreter.cpp
@@ -7,6 +7,7 @@
 #include <cmath>
 using namespace std;
 
+// Holds 2 values, value and type
 class Var {
 private:
     string value;
@@ -29,12 +30,33 @@ public:
     }
 };
 
+// Holds contents and indentLevel, which acts as a "pseudoscope"
+class Line {
+    private:
+    string contents;
+    int indentLevel;
+    public:
+    Line(string contents, int indentLevel) {
+        this->contents = contents;
+        this->indentLevel = indentLevel;
+    }
+    string getContents() {
+        return contents;
+    }
+    int getIndentLevel(){
+        return indentLevel;
+    }
+};
+
+// state of program, which is global
 unordered_map<string, Var> s;
 
+// helper function, which returns true if string is in list
 static bool contains(list<string> list, string target) {
     return (find(list.begin(), list.end(), target) != list.end());
 }
 
+// returns true if variable has been defined
 static bool is_in_state(string var_name, unordered_map<string, Var> state) {
     if (state.find(var_name) == state.end()) {
         return false;
@@ -44,6 +66,7 @@ static bool is_in_state(string var_name, unordered_map<string, Var> state) {
     }
 }
 
+// returns variable if it exists in state
 static Var varmap(string var_name, unordered_map<string,  Var> state) {
     if (is_in_state(var_name, state)) {
         
@@ -55,6 +78,7 @@ static Var varmap(string var_name, unordered_map<string,  Var> state) {
     }    
 }
 
+// strips input of all spaces and newlines
 static string remove_whitespace(string str) {
     str.erase(remove(str.begin(), str.end(), ' '), str.end());
     str.erase(std::remove(str.begin(), str.end(), '\n'), str.end());
@@ -62,6 +86,7 @@ static string remove_whitespace(string str) {
     return str;
 }
 
+// returns true if string can be converted into a postive integer or 0
 bool is_number(const string &s) {
   for (char c: s) {
     string digits = "0123456789";
@@ -72,6 +97,7 @@ bool is_number(const string &s) {
   return true;
 }
 
+// converts string representations of numbers into an int. Only works for zero and positive integers
 static int MDEC(string number) {
     if (is_number(number)) {
         int sum = 0;
@@ -118,15 +144,17 @@ static int MDEC(string number) {
     }
 }
 
+// parses an expression into operators and identifier tokens
 static list<string> parse_expr(string expr) {
     list<string> tokens;
     string operators = "+-*/%()<>";
-    string multi_operators[] = {"&&", "||", "==", "<=", ">=", "!="};
+    list<string> multi_operators = {"&&", "||", "==", "<=", ">=", "!="};
     expr = remove_whitespace(expr);
     //parse expression into tokens
     int index = 0;
     while (expr.length() != 0) {
-        if (expr.substr(index, 2) == multi_operators[0] || expr.substr(index, 2) == multi_operators[1] || expr.substr(index, 2) == multi_operators[2]){
+        // check for two character long operators
+        if (contains(multi_operators, expr.substr(index, 2))){
             if (index > 0) {
                 tokens.push_back(expr.substr(0, index));
             }
@@ -134,6 +162,7 @@ static list<string> parse_expr(string expr) {
             expr = expr.substr(index+2,expr.length() - 1 - index);
             index = 0;
         }
+        // check for single character operators
         else if (operators.find(expr.substr(index, 1)) != string::npos) {
             if (index > 0) {
                 tokens.push_back(expr.substr(0, index));
@@ -142,52 +171,56 @@ static list<string> parse_expr(string expr) {
             expr = expr.substr(index+1,expr.length() - 1 - index);
             index = 0;
         }
+        // check if expression is just a boolean, is a variable in program state, or is a string TODO: this logic is lazy and wrong
         else if (expr == "true" || expr == "false" || is_in_state(expr, s) || is_number(expr) || (expr.find('\"') == 0 && expr.rfind('\"') == expr.length()-1)) {
             tokens.push_back(expr);
             expr = "";
         }
         else {
-            //TODO: Something here because this is an error I think
+            //TODO: this is sometimes an infinite loop. 
             index++;
         }
-        // for (string i : tokens) {
-        //     cout << i << ", ";
-        // }
-        // cout << endl;
     }
     return tokens;
 }
 
+// determines type of single value
 static string determine_type(string value) {
+    // variable
     if (is_in_state(value, s)) {
         return varmap(value, s).getType();
     }
+    // boolean literal
     if (value == "true" || value == "false") {
         return "bool";
     }
+    // integer
     else if (is_number(value)) {
         return "int";
     }
+    // string
     else if (value[0] == '"' && value[value.length()-1] == '"') {
         return "string";
     }
+    // invalid
     else {
         throw runtime_error("\"" + value + "\" is not of valid type");
     }
 }
-
+// determines type for a parsed expression
 static string determine_type(list<string> tokens) {
     // initial guess based on first token
     string initial_guess = determine_type(tokens.front());
     // automatically determines return type to be bool in the case that a bool operator exists
     for (string token : tokens) {
-        if (token == "&&" || token == "||" || token == ">=" || token == "<=" || token == "!=" || token == ">" || token == "<") {
+        if (token == "&&" || token == "||" || token == ">=" || token == "<=" || token == "=="|| token == "!=" || token == ">" || token == "<") {
             return "bool";
         }
     }
     return initial_guess;
 }
 
+// works with evaluate_expression to recursively work through parentheses
 static list<string> evaluate_parentheses(list<string> tokens) {
     //TODO: implement stack for dealing with parentheses, calling evaluate_expr on the insides of the parentheses
         // int startIndex = distance(tokens.begin(), find(tokens.begin(), tokens.end(), "("));
@@ -200,20 +233,19 @@ static list<string> evaluate_parentheses(list<string> tokens) {
         //         string elemnt = *it;
         //         it = tokens.erase(it);
         // }
-        // for (string i : tokens) {
-        //     cout << i << ", ";
-        // }
-        // cout << endl;
     return tokens;
 }
 
+// determines the value of a set of parsed tokens
 static string evaluate_expr(list<string> tokens) {
+    //check if token is in varmap
+    for (string& token : tokens) {
+        if (is_in_state(token, s)) {
+            token = varmap(token, s).getVal();
+         }
+    }
     // determine return type
     string return_type = determine_type(tokens);
-    // for (string t: tokens) {
-    //     cout << t << ", ";
-    // }
-    // cout << endl;
     // base case, there is a single token
     if (tokens.size() == 1) {
         string token = tokens.front();
@@ -228,12 +260,10 @@ static string evaluate_expr(list<string> tokens) {
     if (contains(tokens, "(") || contains(tokens, ")")) {
         tokens = evaluate_parentheses(tokens);
     }
-
     // iterative case: evaluate operations in correct precedence
     if (return_type != "string") {
         auto it = tokens.begin();
         while (contains(tokens, "*") || contains(tokens, "/") || contains(tokens, "%")) {
-            //Identify if current index is an operator
             string token = *it;
             if (token == "*") {
                 int operand1 = MDEC(*prev(it));
@@ -308,17 +338,12 @@ static string evaluate_expr(list<string> tokens) {
             else if (token == ">=") {
                 int operand1 = MDEC(*prev(it));
                 int operand2 = MDEC(*next(it));
-                *it = (operand1 > operand2) ? "true" : "false";
+                *it = (operand1 >= operand2) ? "true" : "false";
                 tokens.erase(prev(it));
                 tokens.erase(next(it));
             }
-            for (string t: tokens) {
-                cout << t << ", ";
-            }
-            cout << endl;
             advance(it, 1);
         }
-        it = tokens.begin();
         while(contains(tokens, "==") || contains(tokens, "!=")) {
             string token = *it;
             if (token == "==") {
@@ -335,7 +360,7 @@ static string evaluate_expr(list<string> tokens) {
                 string operand2 = *next(it);
                 string type1 = determine_type(operand1);
                 string type2 = determine_type(operand2);
-                *it = (operand1 != operand2) ? "true" : "false";
+                *it = (type1 != type2 || operand1 != operand2) ? "true" : "false";
                 tokens.erase(prev(it));
                 tokens.erase(next(it));
             }
@@ -392,57 +417,132 @@ static string evaluate_expr(list<string> tokens) {
         return tokens.front();
     }
     else {
-        throw runtime_error("expression does not evaluate to any valid value");
+        throw runtime_error("invalid expression");
     }
 }
 
+// overloaded version of evaluate_expr which parses the expression first
 static string evaluate_expr(string expr) {
         // parse expression
         list<string> tokens = parse_expr(expr);
-        // TODO: determine if all tokens are valid
-        for (string& token : tokens) {
-            if (is_in_state(token, s)) {
-               token = varmap(token, s).getVal();
-            }
         // call sister-function
-        }
         return evaluate_expr(tokens);
 }
 
-static void execute(string line) {
-    if (line.length() >= 4 && line.rfind("let ") == 0) {
-        line = remove_whitespace(line.substr(4));
-        string var = line.substr(0, line.find("="));
-        string expr = line.substr(line.find("=") + 1);
-        string val = evaluate_expr(expr);
-        s[var] = Var(val, determine_type(val));
-    }
-    else if (line.length() >= 5 && line.rfind("print" == 0)) {
-        line = line.substr(line.rfind("(") + 1, line.find(")") - line.rfind("(")-1);
-        cout << evaluate_expr(line) << endl;
-    }
-    else {
-        throw runtime_error("\"" + line + "\" is not a valid or known statement\n");
-    }
-}
+// executes all lines, is recursive
+static int execute(list<Line> lines, int currentLevel) {
+    int lines_executed = 0;
+    auto it = lines.begin();
+    
+    while (it != lines.end()) {
+        // unpack line contents and remove leading whitespace
+        Line line = *it;
+        string contents = line.getContents();
+        contents = contents.substr(contents.find_first_not_of(" "));
+        int indentLevel = line.getIndentLevel();
+        
+        // scope checks
+        if (indentLevel > currentLevel) {
+            it++;
+            continue;
+        }
+        else if (indentLevel < currentLevel) {
+            break;
+        }
+        else {
+            lines_executed++;
+        }
 
-static void interpret(string filepath) {
-
+        // if statement
+        if (contents.length() >= 2 && contents.rfind("if") == 0) {
+            contents = contents.substr(contents.rfind("(") + 1, contents.find(")") - contents.rfind("(")-1);
+            if (evaluate_expr(contents) == "true") {
+                list<Line> ifLines(next(it), lines.end());
+                int lines_to_skip = execute(ifLines, currentLevel + 1);
+                advance(it, lines_to_skip);
+                continue;
+            }
+        }
+        // while loop
+        else if (contents.length() >= 5 && contents.rfind("while") == 0) {
+            contents = contents.substr(contents.rfind("(") + 1, contents.find(")") - contents.rfind("(")-1);
+            if (evaluate_expr(contents) == "true") {
+                list<Line> whileLines(next(it), lines.end());
+                execute(whileLines, currentLevel + 1);
+                continue;
+            }
+        }
+        // for loop
+        else if (contents.length() >= 5 && contents.rfind("while") == 0) {
+            contents = contents.substr(contents.rfind("(") + 1, contents.find(")") - contents.rfind("(")-1);
+            if (evaluate_expr(contents) == "true") {
+                list<Line> whileLines(next(it), lines.end());
+                execute(whileLines, currentLevel + 1);
+                continue;
+            }
+        }
+        // assignment
+        else if (contents.length() >= 4 && contents.rfind("let ") == 0) {
+            contents = remove_whitespace(contents.substr(4));
+            string var = contents.substr(0, contents.find("="));
+            string expr = contents.substr(contents.find("=") + 1);
+            string val = evaluate_expr(expr);
+            s[var] = Var(val, determine_type(val));
+        }
+        //print statement
+        else if (contents.length() >= 5 && contents.rfind("print") == 0) {
+            contents = contents.substr(contents.rfind("(") + 1, contents.find(")") - contents.rfind("(")-1);
+            cout << evaluate_expr(contents) << endl;
+        }
+        //invalid statement
+        else {
+            throw runtime_error("\"" + contents + "\" is not a valid or known statement\n");
+        }
+        it++;
+    }
+    return lines_executed;
 }
 
 int main(int argc, char *argv[]) {
-
     if (argc >= 1) {
         string filepath = "test.math"; //argv[1];
         // read file
-        ifstream lines(filepath);
-
-        // read lines into line
+        ifstream inputFile(filepath);
+        // put lines into list object
+        list<Line> lines;
         string line;
-        while (getline (lines, line)) {
-            execute(line);
+        // determine scope
+        while (getline(inputFile, line)) {
+            if (line.empty()) {
+                continue;
+            }
+            int indentLevel = -1;
+            if (line[0] != ' ') {
+                indentLevel = 0;
+            }
+            else if (line.find_first_not_of(" ") > lines.back().getContents().find_first_not_of(" ")) {
+                indentLevel = lines.back().getIndentLevel() + 1;
+            }
+            else if (line.find_first_not_of(" ") == lines.back().getContents().find_first_not_of(" ")) {
+                indentLevel = lines.back().getIndentLevel();
+            }
+            else {
+                for (auto it = lines.rbegin(); it != lines.rend(); ++it) {
+                    Line currentComparison = *it;
+                    if (line.find_first_not_of(" ") == currentComparison.getContents().find_first_not_of(" ")) {
+                        indentLevel = currentComparison.getIndentLevel();
+                    }   
+                }
+                if (indentLevel < 0) {
+                    throw runtime_error("invalid indentation syntax");
+                }
+            }
+            // add to lines list
+            lines.push_back(Line(line, indentLevel));  
         }
-    }
+
+        execute(lines, 0);
+    } 
     else {
         cout << argc - 1 << " args recieved, expected 1\n";
     }
